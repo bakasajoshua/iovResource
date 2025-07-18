bfbe5e7e70d5e4ce6ec24d7180460f29a42fae1b
bakasajoshua <baksajoshua09@gmail.com>
2025-07-18 20:18:15 +0300

    Added app short name

--- config/app.php
+++ config/app.php
@@ -17,6 +17,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'short_name' => env('APP_SHORT_NAME', 'Laravel'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
